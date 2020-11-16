5c1c75774eae918eb82ad5fa60bfee1ff3c8f45d
Damian Grande <damian.grande@alimentiamoci.com>
2020-11-16 14:10:09 +0100

    2-72

--- main/views.py
+++ main/views.py
@@ -10,56 +10,56 @@ def home_view(request):
             'events': [
                 {
                     'details': 'Get rid of pointless stuff.',
-                    'edit': False
+                    'edit': 'False'
                 },
                 {
                     'details': 'Try to buy PlayStation 5.',
-                    'edit': False
+                    'edit': 'False'
                 }
             ],
-            'active': True
+            'active': 'True'
         },
         {
             'id': 'day 2',
             'abbvTitle': 'Tue',
             'fullTitle': 'Tuesday',
             'events': [],
-            'active': True
+            'active': 'True'
         },
         {
             'id': 'day 3',
             'abbvTitle': 'Wen',
             'fullTitle': 'Wednesday',
             'events': [],
-            'active': True
+            'active': 'True'
         },
         {
             'id': 'day 4',
             'abbvTitle': 'Thu',
             'fullTitle': 'Thursday',
             'events': [],
-            'active': True
+            'active': 'True'
         },
         {
             'id': 'day 5',
             'abbvTitle': 'Fri',
             'fullTitle': 'Friday',
             'events': [],
-            'active': True
+            'active': 'True'
         },
         {
             'id': 'day 6',
             'abbvTitle': 'Sat',
             'fullTitle': 'Saturday',
             'events': [],
-            'active': True
+            'active': 'True'
         },
         {
             'id': 'day 7',
             'abbvTitle': 'Sun',
             'fullTitle': 'Sunday',
             'events': [],
-            'active': True
+            'active': 'True'
         }
     ]
     return render(request, 'main/calendarComponents.html', {'days': days})
